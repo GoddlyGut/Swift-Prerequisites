eabed4f9bb2ad7bc5749c128d9df2f7c1396908c
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2022-08-20 20:55:32 -0400

    Update Decoder.swift

--- JSON Decoder/Decoder.swift
+++ JSON Decoder/Decoder.swift
@@ -1,21 +1,23 @@
-import UIKit
+import Foundation
 
-extension Bundle {
-    func decode<T: Decodable>(_ type: T.Type, from file: String) -> T {
-        guard let url = self.url(forResource: file, withExtension: nil) else {
-            fatalError("Failed to locate \(file) in bundle.")
-        }
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
 
-        guard let data = try? Data(contentsOf: url) else {
-            fatalError("Failed to load \(file) from bundle.")
-        }
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
 
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
         let decoder = JSONDecoder()
-
-        guard let loaded = try? decoder.decode(T.self, from: data) else {
-            fatalError("Failed to decode \(file) from bundle.")
-        }
-
-        return loaded
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
